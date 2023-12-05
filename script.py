158781e738dacd2e64991ae6f2151362d1646ca8
pedromgcatarino <pm.catarino@campus.fct.unl.pt>
2023-12-05 13:24:23 +0000

    Update project

--- script.py
+++ script.py
@@ -1,22 +1,28 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.feature_selection import SelectKBest
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split, KFold
+from sklearn.model_selection import KFold
 from sklearn.manifold import Isomap
 from sklearn.impute import KNNImputer
-from sklearn.impute import SimpleImputer
-from sklearn.experimental import enable_iterative_imputer
 from sklearn.linear_model import LinearRegression, LogisticRegression, Ridge
-from sklearn.neighbors import KNeighborsRegressor
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.pipeline import Pipeline, make_pipeline
-from sklearn.impute import IterativeImputer
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.model_selection import ShuffleSplit
-from sklearn.feature_selection import SelectKBest, f_classif
-from sklearn.decomposition import PCA
-import xgboost as xgb
+
+from sklearn.decomposition import PCA, KernelPCA
+
+def plot_graph(training_errors, validation_errors, xlabel):
+    xvals = list(range(1, len(training_errors) + 1))
+    plt.figure(figsize=(10, 6))
+    plt.plot(xvals, training_errors, label='Training Error', color='blue')
+    plt.plot(xvals, validation_errors, label='Cross-Validation Error', color='red')
+    plt.title('Training vs Cross-Validation Error')
+    plt.xlabel(xlabel)
+    plt.ylabel('Censored Mean Squared Error')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+
 def cMSE(y_hat, y, c):
     err = y - y_hat
     err = (1 - c) * err ** 2 + c * np.maximum(0, err) ** 2
@@ -31,12 +37,12 @@ def calc_fold(X,Y, c, train_ix,valid_ix, pipe):
     error = cMSE(pred, Y[valid_ix], c[valid_ix])
     return error, trainError
 
+
 data = pd.read_csv('./train_data.csv', index_col=0)
-
-inputers = [KNNImputer(n_neighbors=8, weights="uniform"), SimpleImputer(), IterativeImputer()]
-models = [LinearRegression(), Ridge(alpha=10), KNeighborsRegressor(n_neighbors=7)]
-
 testData = pd.read_csv('./test_data.csv', index_col=0).to_numpy()
+
+#inputers = [KNNImputer(n_neighbors=8, weights="uniform"), SimpleImputer(), IterativeImputer()]
+#models = [LinearRegression(), Ridge(alpha=10, solver='cholesky', tol=0.00001), KNeighborsRegressor(n_neighbors=7)]
 
 x = data.drop(columns=['SurvivalTime', 'Censored']).to_numpy()
 
@@ -46,45 +52,92 @@ y = labeled_data['SurvivalTime'].to_numpy()
 c = labeled_data['Censored'].to_numpy()
 X = labeled_data.drop(columns=['SurvivalTime', 'Censored']).to_numpy()
 
+
 best_err = 100
-best_n = 0
+best_nf = 0
 best_p = 0
 best_alpha = 0
-for nn in range(1, 8):
-    for nc in range(1, 8):
-        for a in range(1, 10):
-            alpha = a/10.0
-            pipe = make_pipeline( KNNImputer(n_neighbors=nn, weights="uniform"), StandardScaler(), PCA(n_components=nc),
-                                 Ridge(alpha=alpha))
+val_errors = []
+train_errors = []
+lambs = np.linspace(0.0001, 30)
+for nf in range(1, 8):
 
-            n_folds = 12
-            kf = KFold(n_splits=n_folds)
-            error = 0
-            trainError = 0
-            for train_ix, valid_ix in kf.split(X):
-                err, trainErr = calc_fold(X, y, c, train_ix, valid_ix, pipe)
-                error = error + err
-                trainError = trainError + trainErr
+    # SCALING
+    scaler = StandardScaler()
+    scaler.fit(x)
+    X1 = scaler.transform(X)
+    testData1 = scaler.transform(testData)
 
-            error = error / n_folds
-            trainError = trainError / n_folds
-            if error < best_err:
-                best_err = error
-                best_n = nn
-                best_p = nc
-                best_alpha = alpha
-                test = pipe.predict(testData)
-                t = pd.DataFrame({'TARGET': test})
-                t.to_csv(f'prediction_{nn}_{nc}_{alpha}.csv')
+    # IMPUTING
+    imputer = KNNImputer(n_neighbors=3, weights="uniform")
+    imputer.fit(x)
+    x1 = imputer.transform(x)
+    X2 = imputer.transform(X1)
+    testData2 = imputer.transform(testData1)
 
-            print("CV ERROR FOR N_NEIGHBORS: " + str(nn) + " N_COMPONENTS: " + str(nc) + " ALPHA: " + str(alpha))
-            print(error)
-            print("TRAINING ERROR:")
-            print(trainError)
+    # PCA
+    Xc = X2 - np.mean(X2, axis=0)
+    pca = PCA(n_components=nf)
+    # pca.fit(x)
+    Xc = pca.fit_transform(Xc)
 
+    Xt = testData2 - np.mean(testData2, axis=0)
+    Xt = pca.fit_transform(Xt)
+
+    # Kernel PCA
+    Xc1 = X2 - np.mean(X2, axis=0)
+    transformer = KernelPCA(n_components=nf, kernel='rbf', eigen_solver='dense')
+    # transformer.fit(x)
+    Xc1 = transformer.fit_transform(Xc1)
+
+    Xt1 = testData2 - np.mean(testData2, axis=0)
+    Xt1 = transformer.fit_transform(Xt1)
+
+    # ISOMAP
+    Xc2 = X2 - np.mean(X2, axis=0)
+    embedding = Isomap(n_components=nf)
+    # embedding.fit(x)
+    Xc2 = embedding.fit_transform(Xc2)
+
+    Xt2 = testData2 - np.mean(testData2, axis=0)
+    Xt2 = embedding.fit_transform(Xt2)
+
+    # SELECT K BEST
+    Xb = np.append(Xc, Xc1, axis=1)
+    Xb = np.append(Xb, Xc2, axis=1)
+
+    Xtt = np.append(Xt, Xt1, axis=1)
+    Xtt = np.append(Xtt, Xt2, axis=1)
+
+    skb = SelectKBest(k=nf)
+    Xb = skb.fit_transform(Xb, y)
+    finaltestData = skb.transform(Xtt)
+
+
+    # CROSS-VALIDATION
+    n_folds = 20
+    kf = KFold(n_splits=n_folds)
+    error = 0
+    trainError = 0
+    model = LinearRegression()
+    for train_ix, valid_ix in kf.split(X):
+        err, trainErr = calc_fold(Xb, y, c, train_ix, valid_ix, model)
+        error = error + err
+        trainError = trainError + trainErr
+
+    error = error / n_folds
+    trainError = trainError / n_folds
+    val_errors.append(error)
+    train_errors.append(trainError)
+    if error < best_err:
+        best_err = error
+        best_nf = nf
+        test = model.predict(finaltestData)
+        t = pd.DataFrame({'TARGET': test})
+        t.to_csv(f'prediction_{nf}.csv')
+
+plot_graph(train_errors, val_errors, "N Features")
 
 
 print("BEST ERROR: " + str(best_err))
-print("BEST N: " + str(best_n))
-print("BEST C: " + str(best_p))
-print("BEST ALPHA: " + str(best_alpha))
+print("BEST NF: " + str(best_nf))
